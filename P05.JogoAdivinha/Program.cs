512505b8f0a5f7d88cf974da89bb8bc27e15261a
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-03-29 01:53:24 -0300

    Code refactoring.

--- P05.JogoAdivinha/Program.cs
+++ P05.JogoAdivinha/Program.cs
@@ -4,210 +4,159 @@
      {
           static void Main(string[] args)
           {
-
                Random aleatorio = new Random();
 
                do
                {
-
                     string firula = new string('-', 43);
 
                     int numeroAleatorio = aleatorio.Next(1, 20);
 
-                    #region Menu e opção de sair;
+                    string opcao = GerarMenu();
 
-                    Console.Clear();
-                    Console.WriteLine("===============================");
-                    Console.WriteLine("\nJogo da Adivinhação!\n");
-                    Console.WriteLine("===============================");
-
-                    Console.Write("\nDigite S para sair, ou qualquer outro botão para continuar:\n> ");
-
-                    string op;
-                    op = Console.ReadLine();
-
-
-                    if (op == "s" || op == "S")
+                    if (opcao == "S")
                     {
+                         Console.ForegroundColor = ConsoleColor.Red;
                          Console.WriteLine("\nSaindo...");
+                         Console.ResetColor();
                          break;
                     }
 
-                    #endregion
-
-                    #region Opção de escolha de dificuldade;
-
-                    int nivelDificuldade = 0, tentativas = 0, numeroChute = 0;
+                    int tentativas = 0, numeroChute = 0;
                     int pontuacao = 0;
                     int pontuacaoFinal = 1000;
 
                     Console.WriteLine("\nEscolha o nível de dificuldade");
                     Console.WriteLine("\n(1) Fácil (2) Médio (3) Difícil");
-                    Console.Write("\nEscolha: ");
 
-                    nivelDificuldade = Convert.ToInt32(Console.ReadLine());
-
-                    #endregion
+                    int nivelDificuldade = PegarValor("\nEscolha:\n> ");
 
                     switch (nivelDificuldade)
                     {
-                         #region Dificuldade fácil;
-
                          case 1:
                               tentativas = 15;
 
-                              for (int i = 0; i < tentativas; i++)
-                              {
-                                   Console.Clear();
-                                   Console.WriteLine($"\nTentativa {i + 1} de {tentativas}\n");
-                                   Console.WriteLine(firula);
-                                   
-
-                                   Console.Write($"\nQual o seu {i + 1}º chute? ");
-                                   numeroChute = Convert.ToInt32(Console.ReadLine());
-
-                                   if (numeroChute < numeroAleatorio)
-                                   {
-                                        pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
-                                        pontuacaoFinal = pontuacaoFinal - pontuacao;
-
-                                        Console.WriteLine("Seu chute foi menor que o número secreto!");
-                                        Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
-                                        Console.ReadLine();
-                                   }
-
-                                   if (numeroChute > numeroAleatorio)
-                                   {
-                                        pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
-                                        pontuacaoFinal = pontuacaoFinal - pontuacao;
-                                        Console.WriteLine("Seu chute foi maior que o número secreto!");
-                                        Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
-                                        Console.ReadLine();
-                                   }
-
-                                   if (numeroChute == numeroAleatorio)
-                                   {
-                                        Console.WriteLine("Parabéns, você acertou!");
-                                        Console.WriteLine($"\nPontuação final é de {pontuacaoFinal}!");
-                                        break;                                       
-                                   }
-                                   if (i + 1 >= tentativas)
-                                   {
-                                        Console.WriteLine("Acabaram suas tentativas!");
-                                        Console.ReadLine();
-                                        break;
-                                   }
-                              }
+                              VerificarChute(firula, numeroAleatorio, tentativas, ref numeroChute, ref pontuacao, ref pontuacaoFinal);
 
                               break;
-
-                         #endregion
-
-                         #region Dificuldade Média;
 
                          case 2:
                               tentativas = 10;
 
-                              for (int i = 0; i < tentativas; i++)
-                              {
-                                   Console.Clear();
-                                   Console.WriteLine($"\nTentativa {i + 1} de {tentativas}\n");
-                                   Console.WriteLine(firula);
-                                   
+                              VerificarChute(firula, numeroAleatorio, tentativas, ref numeroChute, ref pontuacao, ref pontuacaoFinal);
 
-                                   Console.Write($"\nQual o seu {i + 1}º chute? ");
-                                   numeroChute = Convert.ToInt32(Console.ReadLine());
-
-                                   if (numeroChute < numeroAleatorio)
-                                   {
-                                        pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
-                                        pontuacaoFinal = pontuacaoFinal - pontuacao;
-
-                                        Console.WriteLine("Seu chute foi menor que o número secreto!");
-                                        Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
-                                        Console.ReadLine();
-                                   }
-
-                                   if (numeroChute > numeroAleatorio)
-                                   {
-                                        pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
-                                        pontuacaoFinal = pontuacaoFinal - pontuacao;
-                                        Console.WriteLine("Seu chute foi maior que o número secreto!");
-                                        Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
-                                        Console.ReadLine();
-                                   }
-
-                                   if (numeroChute == numeroAleatorio)
-                                   {
-                                        Console.WriteLine("Parabéns, você acertou!");
-                                        Console.WriteLine($"\nPontuação final é de {pontuacaoFinal}!");
-                                        break;
-                                   }
-                                   if (i + 1 >= tentativas)
-                                   {
-                                        Console.WriteLine("\nAcabaram suas tentativas!");
-                                        Console.ReadLine();
-                                        break;
-                                   }
-                              }
                               break;
-
-                         #endregion
-
-                         #region Dificuldade Difícil;
 
                          case 3:
                               tentativas = 5;
-                             
-                              for (int i = 0; i < tentativas; i++)
-                              {
-                                   Console.Clear();
-                                   Console.WriteLine($"\nTentativa {i + 1} de {tentativas}\n");
-                                   Console.WriteLine(firula);
-                                   
-                                   Console.Write($"\nQual o seu {i + 1}º chute? ");
-                                   numeroChute = Convert.ToInt32(Console.ReadLine());
 
-                                   if (numeroChute < numeroAleatorio)
-                                   {
-                                        pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
-                                        pontuacaoFinal = pontuacaoFinal - pontuacao;
+                              VerificarChute(firula, numeroAleatorio, tentativas, ref numeroChute, ref pontuacao, ref pontuacaoFinal);
 
-                                        Console.WriteLine("Seu chute foi menor que o número secreto!");
-                                        Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
-                                        Console.ReadLine();
-                                   }
-
-                                   if (numeroChute > numeroAleatorio)
-                                   {
-                                        pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
-                                        pontuacaoFinal = pontuacaoFinal - pontuacao;
-                                        Console.WriteLine("Seu chute foi maior que o número secreto!");
-                                        Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
-                                        Console.ReadLine();
-                                   }
-
-                                   if (numeroChute == numeroAleatorio)
-                                   {
-                                        Console.WriteLine("Parabéns, você acertou!");
-                                        Console.WriteLine($"\nPontuação final é de {pontuacaoFinal}!");
-                                        break;
-                                   }
-                                   if (i + 1 >= tentativas)
-                                   {
-                                        Console.WriteLine("\nAcabaram suas tentativas!");
-                                        Console.ReadLine();
-                                        break;
-                                   }
-                              }
                               break;
-
-                              #endregion
                     }
                     Console.ReadLine();
 
                } while (true);
-          
+
           }
+
+          #region Funçõees;
+          private static void VerificarChute(string firula, int numeroAleatorio, int tentativas, ref int numeroChute, ref int pontuacao, ref int pontuacaoFinal)
+          {
+               for (int i = 0; i < tentativas; i++)
+               {
+                    numeroChute = PegarChute(firula, tentativas, i);
+
+                    if (numeroChute < numeroAleatorio)
+                    {
+                         pontuacao = ChuteMenor(numeroAleatorio, numeroChute, ref pontuacaoFinal);
+                    }
+
+                    if (numeroChute > numeroAleatorio)
+                    {
+                         pontuacao = ChuteMaior(numeroAleatorio, numeroChute, ref pontuacaoFinal);
+                    }
+
+                    if (numeroChute == numeroAleatorio)
+                    {
+                         AcertouChute(pontuacaoFinal);
+                         break;
+                    }
+                    if (i + 1 >= tentativas)
+                    {
+                         AcabouTentativas();
+                         break;
+                    }
+               }
+          }
+
+          private static void AcabouTentativas()
+          {
+               Console.ForegroundColor = ConsoleColor.Red;
+               Console.WriteLine("Acabaram suas tentativas!");
+               Console.ResetColor();
+               Console.ReadLine();
+          }
+
+          private static void AcertouChute(int pontuacaoFinal)
+          {
+               Console.WriteLine("Parabéns, você acertou!");
+               Console.WriteLine($"\nPontuação final é de {pontuacaoFinal}!");
+          }
+
+          private static int ChuteMaior(int numeroAleatorio, int numeroChute, ref int pontuacaoFinal)
+          {
+               int pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
+               pontuacaoFinal = pontuacaoFinal - pontuacao;
+               Console.WriteLine("Seu chute foi maior que o número secreto!");
+               Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
+               Console.ReadLine();
+               return pontuacao;
+          }
+
+          private static int ChuteMenor(int numeroAleatorio, int numeroChute, ref int pontuacaoFinal)
+          {
+               int pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
+               pontuacaoFinal = pontuacaoFinal - pontuacao;
+
+               Console.WriteLine("Seu chute foi menor que o número secreto!");
+               Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
+               Console.ReadLine();
+               return pontuacao;
+          }
+
+          private static int PegarChute(string firula, int tentativas, int i)
+          {
+               int numeroChute;
+               Console.Clear();
+               Console.WriteLine($"\nTentativa {i + 1} de {tentativas}\n");
+               Console.WriteLine(firula);
+
+               numeroChute = PegarValor($"\nQual o seu {i + 1}º chute? ");
+               return numeroChute;
+          }
+
+          private static string GerarMenu()
+          {
+               Console.Clear();
+               Console.WriteLine("===============================");
+               Console.WriteLine("\nJogo da Adivinhação!\n");
+               Console.WriteLine("===============================");
+
+               Console.Write("\nDigite S para sair, ou qualquer outro botão para continuar:\n> ");
+
+               string opcao;
+               opcao = Console.ReadLine().ToUpper();
+               return opcao;
+          }
+
+          private static int PegarValor(string mensagem)
+          {
+               Console.Write(mensagem);
+               int valor = Convert.ToInt32(Console.ReadLine());
+               return valor;
+          }
+
+          #endregion
      }
 }
