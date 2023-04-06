566f65fd4e05655bad31a55f236814f7a8985630
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-04-06 15:00:08 -0300

    Refatoração utilizando Orientação a Objetos.

---
+++ P05.JogoAdivinha/JogoAdivinha.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05.JogoAdivinha
+{
+     internal class JogoAdivinha
+     {
+          public string firula = new string('-', 43);
+    
+          public int tentativas = 0, numeroChute = 0,pontuacaoFinal = 1000 , pontuacao = 0, numeroAleatorio, nivelDificuldade;
+
+          public  void VerificarChute()
+          {
+               for (int i = 0; i < tentativas; i++)
+               {
+                    numeroChute = PegarChute(i);
+
+                    if (numeroChute < numeroAleatorio)
+                    {
+                         pontuacao = ChuteMenor();
+                    }
+
+                    if (numeroChute > numeroAleatorio)
+                    {
+                         pontuacao = ChuteMaior();
+                    }
+
+                    if (numeroChute == numeroAleatorio)
+                    {
+                         AcertouChute();
+                         break;
+                    }
+                    if (i + 1 >= tentativas)
+                    {
+                         AcabouTentativas();
+                         break;
+                    }
+               }
+          }
+
+          public  void AcabouTentativas()
+          {
+               Console.ForegroundColor = ConsoleColor.Red;
+               Console.WriteLine("Acabaram suas tentativas!");
+               Console.ResetColor();
+               Console.ReadLine();
+          }
+
+          public  void AcertouChute()
+          {
+               Console.WriteLine("Parabéns, você acertou!");
+               Console.WriteLine($"\nPontuação final é de {pontuacaoFinal}!");
+          }
+
+          public  int ChuteMaior()
+          {
+               int pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
+               pontuacaoFinal = pontuacaoFinal - pontuacao;
+               Console.WriteLine("Seu chute foi maior que o número secreto!");
+               Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
+               Console.ReadLine();
+               return pontuacao;
+          }
+
+          public  int ChuteMenor()
+          {
+               int pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
+               pontuacaoFinal = pontuacaoFinal - pontuacao;
+
+               Console.WriteLine("Seu chute foi menor que o número secreto!");
+               Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
+               Console.ReadLine();
+               return pontuacao;
+          }
+
+          public  int PegarChute(int i)
+          {
+               int numeroChute;
+               Console.Clear();
+               Console.WriteLine($"\nTentativa {i + 1} de {tentativas}\n");
+               Console.WriteLine(firula);
+
+               numeroChute = PegarValor($"\nQual o seu {i + 1}º chute? ");
+               return numeroChute;
+          }
+
+          public  int PegarValor(string mensagem)
+          {
+               Console.Write(mensagem);
+               int valor = Convert.ToInt32(Console.ReadLine());
+               return valor;
+          }
+
+          public void GerarJogoAdivinha()
+          {
+               switch (nivelDificuldade)
+               {
+                    case 1:
+                         tentativas = 15;
+
+                         VerificarChute();
+
+                         break;
+
+                    case 2:
+                         tentativas = 10;
+
+                         VerificarChute();
+
+                         break;
+
+                    case 3:
+                         tentativas = 5;
+
+                         VerificarChute();
+
+                         break;
+               }
+          }
+     }
+}
--- P05.JogoAdivinha/Program.cs
+++ P05.JogoAdivinha/Program.cs
@@ -4,139 +4,25 @@
      {
           static void Main(string[] args)
           {
-               Random aleatorio = new Random();
-
                do
                {
-                    string firula = new string('-', 43);
-
-                    int numeroAleatorio = aleatorio.Next(1, 20);
-
-                    string opcao = GerarMenu();
-
-                    if (opcao == "S")
-                    {
-                         Console.ForegroundColor = ConsoleColor.Red;
-                         Console.WriteLine("\nSaindo...");
-                         Console.ResetColor();
-                         break;
-                    }
-
-                    int tentativas = 0, numeroChute = 0;
-                    int pontuacao = 0;
-                    int pontuacaoFinal = 1000;
+                    JogoAdivinha jogo1 = new JogoAdivinha();
+                    Random aleatorio = new Random();
+                    jogo1.numeroAleatorio = aleatorio.Next(1, 20);
+                    GerarMenu();
 
                     Console.WriteLine("\nEscolha o nível de dificuldade");
                     Console.WriteLine("\n(1) Fácil (2) Médio (3) Difícil");
 
-                    int nivelDificuldade = PegarValor("\nEscolha:\n> ");
+                    jogo1.nivelDificuldade = jogo1.PegarValor("\nEscolha:\n> ");
+                    jogo1.GerarJogoAdivinha();
 
-                    switch (nivelDificuldade)
-                    {
-                         case 1:
-                              tentativas = 15;
-
-                              VerificarChute(firula, numeroAleatorio, tentativas, ref numeroChute, ref pontuacao, ref pontuacaoFinal);
-
-                              break;
-
-                         case 2:
-                              tentativas = 10;
-
-                              VerificarChute(firula, numeroAleatorio, tentativas, ref numeroChute, ref pontuacao, ref pontuacaoFinal);
-
-                              break;
-
-                         case 3:
-                              tentativas = 5;
-
-                              VerificarChute(firula, numeroAleatorio, tentativas, ref numeroChute, ref pontuacao, ref pontuacaoFinal);
-
-                              break;
-                    }
                     Console.ReadLine();
 
                } while (true);
 
           }
-
-          #region Funçõees;
-          private static void VerificarChute(string firula, int numeroAleatorio, int tentativas, ref int numeroChute, ref int pontuacao, ref int pontuacaoFinal)
-          {
-               for (int i = 0; i < tentativas; i++)
-               {
-                    numeroChute = PegarChute(firula, tentativas, i);
-
-                    if (numeroChute < numeroAleatorio)
-                    {
-                         pontuacao = ChuteMenor(numeroAleatorio, numeroChute, ref pontuacaoFinal);
-                    }
-
-                    if (numeroChute > numeroAleatorio)
-                    {
-                         pontuacao = ChuteMaior(numeroAleatorio, numeroChute, ref pontuacaoFinal);
-                    }
-
-                    if (numeroChute == numeroAleatorio)
-                    {
-                         AcertouChute(pontuacaoFinal);
-                         break;
-                    }
-                    if (i + 1 >= tentativas)
-                    {
-                         AcabouTentativas();
-                         break;
-                    }
-               }
-          }
-
-          private static void AcabouTentativas()
-          {
-               Console.ForegroundColor = ConsoleColor.Red;
-               Console.WriteLine("Acabaram suas tentativas!");
-               Console.ResetColor();
-               Console.ReadLine();
-          }
-
-          private static void AcertouChute(int pontuacaoFinal)
-          {
-               Console.WriteLine("Parabéns, você acertou!");
-               Console.WriteLine($"\nPontuação final é de {pontuacaoFinal}!");
-          }
-
-          private static int ChuteMaior(int numeroAleatorio, int numeroChute, ref int pontuacaoFinal)
-          {
-               int pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
-               pontuacaoFinal = pontuacaoFinal - pontuacao;
-               Console.WriteLine("Seu chute foi maior que o número secreto!");
-               Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
-               Console.ReadLine();
-               return pontuacao;
-          }
-
-          private static int ChuteMenor(int numeroAleatorio, int numeroChute, ref int pontuacaoFinal)
-          {
-               int pontuacao = Math.Abs((numeroChute - numeroAleatorio) / 2);
-               pontuacaoFinal = pontuacaoFinal - pontuacao;
-
-               Console.WriteLine("Seu chute foi menor que o número secreto!");
-               Console.WriteLine($"Você fez {pontuacaoFinal} pontos!");
-               Console.ReadLine();
-               return pontuacao;
-          }
-
-          private static int PegarChute(string firula, int tentativas, int i)
-          {
-               int numeroChute;
-               Console.Clear();
-               Console.WriteLine($"\nTentativa {i + 1} de {tentativas}\n");
-               Console.WriteLine(firula);
-
-               numeroChute = PegarValor($"\nQual o seu {i + 1}º chute? ");
-               return numeroChute;
-          }
-
-          private static string GerarMenu()
+          private static void GerarMenu()
           {
                Console.Clear();
                Console.WriteLine("===============================");
@@ -147,16 +33,14 @@
 
                string opcao;
                opcao = Console.ReadLine().ToUpper();
-               return opcao;
-          }
 
-          private static int PegarValor(string mensagem)
-          {
-               Console.Write(mensagem);
-               int valor = Convert.ToInt32(Console.ReadLine());
-               return valor;
+               if (opcao == "S")
+               {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nSaindo...");
+                    Console.ResetColor();
+                    Environment.Exit(0);
+               }
           }
-
-          #endregion
      }
 }
